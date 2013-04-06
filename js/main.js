78db416362e01914d0dd9c1bbdaa246a35d5d4ff
FajitaNachos <andrew@fajitanachos.com>
2013-04-06 21:50:40 +0900

    Add home page.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+jQuery(function($){
+	$("#about-link").on('click', function(){
+		if($("#contact").css('display') == 'block'){
+			$("#contact").hide();
+		}
+		$("#home-image").hide();
+		$("#intro").hide();
+		$("#about").fadeIn('fast');
+		
+		
+	});
 
+	$("#contact-link").on('click', function(){
+		if($("#about").css('display') == 'block'){
+			$("#about").hide();
+		}
+		if($("#intro").css('display') == 'block'){
+			$("#intro").hide();
+		}
+		$("#home-image").show();
+		$("#contact").fadeIn('fast');
+
+	});
+
+});
