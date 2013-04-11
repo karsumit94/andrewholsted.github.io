a6987be07d2dd01d691a390a41486c4e8ca05310
FajitaNachos <andrew@fajitanachos.com>
2013-04-11 21:51:01 +0900

    Update styles and layout.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 jQuery(function($){
-	$("#about-link").on('click', function(){
-		
+	
+	$("#about-link").on('click', function(e){
+		e.preventDefault();
+		$(document.body).animate({
+    		'scrollTop':   $('#about').offset().top
+		}, 1000);
 		
 	});
 
-	$("#contact-link").on('click', function(){
-		
-
-	});
-
-	$("#posts-link").on('click', function(){
-		
+	$("#projects-link").on('click', function(e){
+		e.preventDefault();
+		$(document.body).animate({
+    		'scrollTop':   $('#projects').offset().top
+		}, 1000);
 	
 	});
+
+	$("#posts-link").on('click', function(e){
+		e.preventDefault();
+		$(document.body).animate({
+    		'scrollTop':   $('#posts').offset().top
+		}, 1000);
 	
-	$("#projects-link").on('click', function(){
-		
+	});
 	
+	$("#contact-link").on('click', function(e){
+		e.preventDefault();
+		$(document.body).animate({
+    		'scrollTop':   $('#contact').offset().top
+		}, 1000);
+
 	});
 
 
 });
+
+
