8bc3fc0e4ed6a8eea378bff6f12da5e49da231bf
FajitaNachos <andrew@fajitanachos.com>
2013-04-14 07:45:24 +0900

    Add basic content slider.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-jQuery(function($){
+$(function($){
 	
 	$("#about-link").on('click', function(e){
 		e.preventDefault();
@@ -31,8 +31,57 @@ jQuery(function($){
 		}, 1000);
 
 	});
+ 
+	// slider
+    if($("#slider").length){
 
+    	// Set the width of each slide to the widow width;
+        $("#slider li").width($(window).innerWidth());
+        // Declare variables
+        var sliderLength = $("#slider > li").length,
+        	currentPosition = 1,
+            slideWidth = $("#slider > li:first").outerWidth(true),
+            totalWidth = slideWidth * sliderLength;
+         
+        $("#slider").width(totalWidth);
 
+        //hide the slider-prev control on initial load.
+        manageSlideControls(currentPosition,sliderLength);
+
+        $("#slider-prev").click(function(){
+            if(currentPosition !== 1){
+                $("#slider").animate({left : "+=" + slideWidth + "px"});
+           		currentPosition -= 1;
+           		manageSlideControls(currentPosition,sliderLength);
+
+            }
+            return false;
+        });
+        
+        $("#slider-next").click(function(){
+            if(currentPosition !== sliderLength){
+                $("#slider").animate({left : "-=" + slideWidth + "px"});
+                currentPosition += 1;
+                manageSlideControls(currentPosition,sliderLength);
+            }
+            return false;
+        });
+    }
+
+    function manageSlideControls(slidePosition, sliderLength) {
+    	if(slidePosition == 1) {
+    		 $("#slider-prev").hide();
+    	}
+    	else{
+    		$("#slider-prev").show();
+    	}
+    	if(slidePosition == sliderLength) {
+    		$("#slider-next").hide();
+    	}
+    	else{
+    		$("#slider-next").show();
+    	}
+    }
 });
 
 
